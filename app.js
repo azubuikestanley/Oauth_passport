250cfa1006cdf0a9d004c6e462b2d39e7da3f71d
azubuikestanley <azubuikestanley@gmail.com>
2020-01-25 05:22:43 +0100

    auth routes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
 
 const app = express();
 
 
 //setup view engine
 app.set('view engine', 'ejs');
+
+//set up routes
+app.use('/auth', authRoutes);
 
 //create home route
 app.get('/', (req, res) => {
