d265fca18450beefd39feeac8338405dbcfb5dc1
azubuikestanley <azubuikestanley@gmail.com>
2020-01-25 10:44:48 +0100

    added google strategy, google console

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,13 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const keys = require('./key');
 
 passport.use(new GoogleStrategy({
     //options for the google strat
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret
     }), () => {
     //passport callback function
     }
 )
+
