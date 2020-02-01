ac15d0844f2f35b4265cec4def82044384933a44
azubuikestanley <azubuikestanley@gmail.com>
2020-02-01 17:17:32 +0100

    save user to db

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
 
 //setup view engine
 app.set('view engine', 'ejs');
+
+mongoose
+    .connect(keys.mongodb.dbURI,{ 
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false, 
+        useUnifiedTopology: true
+        }, () => {
+            console.log('connected to mongoDB...');
+    });
 
 //set up routes
 app.use('/auth', authRoutes);
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,18 +1,26 @@
 const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+//does same as above
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+const User = require('../models/user-models'); 
 
 passport.use(new GoogleStrategy({
     //options for the google strat
+    callbackURL: '/auth/google/redirect',
     clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret,
-    callbackURL: '/auth/google/redirect'
+    clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
     //passport callback function
-    console.log(profile);
+    console.log(profile); 
+    new User({
+        username: profile.displayName,
+        googleId: profile.id
+    }).save().then((newUser) => {
+        console.log('new user created:' + newUser);
+    });
     // User.findOrCreate({ googleId: profile.id }, (err, user) => {
     //      return done(err, user);
     })
-)
+) 
 
-// module.exports = passport;
