f39a3767d0e30d7105eb7b7089bc3cbeda76ce43
azubuikestanley <azubuikestanley@gmail.com>
2020-02-01 18:28:08 +0100

    check if user is logged in or not and redirect to login if user is not logged in

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
@@ -35,6 +36,7 @@ mongoose
 
 //set up routes
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 // app.use(require('./config/passport-setup'));
 
 
